2248bb75da98a00f9b94d09a95762db622454fa9
owo93 <iztyle30@gmail.com>
2026-02-21 00:19:48 +0700

    initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,21 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from "astro/config";
+
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    vite: {
+        plugins: [tailwindcss()],
+    },
+
+    experimental: {
+        fonts: [
+            {
+                provider: fontProviders.google(),
+                name: "Inter",
+                cssVariable: "--font-inter",
+            },
+        ],
+    },
+});
